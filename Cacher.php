7432c848a0068edfa032fd2f5229acb76ce36736
bart <bart@debian>
2017-02-21 20:38:26 +0100

    Updated formatting

--- Cacher.php
+++ Cacher.php
@@ -10,18 +10,18 @@ class Cacher {
     private $file = "";
     private $cacheDir = "";
 
-    function __construct($file){
+    function __construct($file) {
         $this->cacheDir = $_SERVER["DOCUMENT_ROOT"] . "/cache";
-        if (!file_exists($this->cacheDir)) {
+        if(!file_exists($this->cacheDir)) {
             mkdir($this->cacheDir, 0755, true);
         }
         $this->loadCache($file);
         $this->removeExpired();
     }
     
-    function loadCache($file){
+    function loadCache($file) {
         $this->file = $this->cacheDir . "/" . $file . ".xml";      
-        if(!file_exists($this->file)){
+        if(!file_exists($this->file)) {
             $content = '<?xml version="1.0" encoding="UTF-8"?><items></items>';
             $fp = fopen($this->file,"wb");
             fwrite($fp,$content);
@@ -29,31 +29,31 @@ class Cacher {
         }
     }
 
-    function removeExpired(){
+    function removeExpired() {
         $xmlStr = file_get_contents($this->file);
         $xml = new \SimpleXMLElement($xmlStr);
         $items = $xml->xpath("//item[(date < " . time() . ")]");
-        foreach ($items as $item) {
+        foreach($items as $item) {
             $node = dom_import_simplexml($item);
             $node->parentNode->removeChild($node);
         }
         file_put_contents($this->file, $xml->asXML());
     }
 
-    function getEntry($id){
+    function getEntry($id) {
         $xmlStr = file_get_contents($this->file);
         $xml = new \SimpleXMLElement($xmlStr);
         $res = $xml->xpath("//item[@id = '" . $id . "']");
-        if($res){
+        if($res) {
             $val = (string) $res[0]->content;
-            if($val){
+            if($val) {
                 return unserialize(base64_decode($val));
             }
         }
         return null;
     }
 
-    function addEntry($id, $content, $expirationSeconds){
+    function addEntry($id, $content, $expirationSeconds) {
         $content = base64_encode(serialize($content));
         $xmlStr = file_get_contents($this->file);
         $xml = new \SimpleXMLElement($xmlStr);
@@ -65,9 +65,9 @@ class Cacher {
         file_put_contents($this->file, $xml->asXML());
     }
 
-    function checkExistsAndRemove($xml, $id){
+    function checkExistsAndRemove($xml, $id) {
         $item = $xml->xpath("//item[@id='" . $id . "']");
-        if(isset($item[0])){
+        if(isset($item[0])) {
             $node = dom_import_simplexml($item[0]);
             $node->parentNode->removeChild($node);
         }
--- Example.php
+++ Example.php
@@ -2,7 +2,7 @@
 require_once("Cacher.php");
 $cacher = new Bartronix\Cacher("data");
 $testdata = $cacher->getEntry("testdata");
-if(!$testdata){
+if(!$testdata) {
     $testdata = array("a" => "a", "b" => "b");
     $cacher->addEntry("testdata", $testdata, 20000);	
 }
